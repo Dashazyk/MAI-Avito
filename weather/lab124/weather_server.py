56b04d8d7c20c6cb49304df39a3408c35755d575
Daria Zykova <dashazyk@gmail.com>
2023-05-18 09:55:14 +0300

    Чт 18 мая 2023 09:55:14 MSK

--- weather/lab124/weather_server.py
+++ weather/lab124/weather_server.py
@@ -85,7 +85,7 @@ class WeatherServer():
             city = request.args.get('city', None)
             days = request.args.get('days', None)
             
-            date = datetime.datetime.today() + datetime.timedelta(days=days if days else 0)
+            date = datetime.datetime.today() + datetime.datetime.timedelta(days=days if days else 0)
 
             unam = request.headers.get('Own-Auth-UserName', None)
             self.myredis.set(str(city)+str(date), request.data)
